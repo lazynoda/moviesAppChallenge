56f17e8725f2f2068fd1ad96884e3f4e5793d897
David Martín de Castro <david.martin.decastro@bbva.com>
2018-08-30 17:13:11 +0200

    feature: Wishlist repository and storage data source.

---
+++ app/src/main/java/de/mytoysgroup/movies/challenge/data/repository/wishlist/WishlistRepository.kt
@@ -0,0 +1,17 @@
+package de.mytoysgroup.movies.challenge.data.repository.wishlist
+
+import android.content.Context
+
+class WishlistRepository private constructor(private val storageDataSource: StorageDataSource) {
+
+    constructor(context: Context) : this(StorageDataSource(context))
+
+    fun add(movieId: String) =
+            storageDataSource.add(movieId)
+
+    fun remove(movieId: String) =
+            storageDataSource.remove(movieId)
+
+    fun getAll() =
+            storageDataSource.getAll()
+}
