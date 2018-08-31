82f6fc39dea5ab7946cbb9a7e3450352a36ce83a
David Martín de Castro <david.martin.decastro@bbva.com>
2018-08-31 07:49:46 +0200

    feature: Add wishlist flag to Movie data class.

--- app/src/main/java/de/mytoysgroup/movies/challenge/domain/model/Movie.kt
+++ app/src/main/java/de/mytoysgroup/movies/challenge/domain/model/Movie.kt
@@ -5,6 +5,7 @@ import de.mytoysgroup.movies.challenge.domain.DataMapper
 
 data class Movie(val id: String,
                  val title: String,
+                 val wishlist: Boolean,
                  val poster: Uri,
                  val description: String? = null,
                  val website: Uri? = null,
@@ -14,6 +15,7 @@ data class Movie(val id: String,
         override fun fromMap(map: Map<String, Any?>) = Movie(
                 map["id"] as String,
                 map["title"] as String,
+                map["wishlist"] as Boolean,
                 Uri.parse(map["poster"] as String),
                 map["description"] as? String?,
                 (map["website"] as? String?)?.let(Uri::parse),
@@ -23,6 +25,7 @@ data class Movie(val id: String,
         override fun toMap(value: Movie) = mapOf(
                 "id" to value.id,
                 "title" to value.title,
+                "wishlist" to value.wishlist,
                 "poster" to value.poster.toString(),
                 "description" to value.description,
                 "website" to value.website?.toString(),
